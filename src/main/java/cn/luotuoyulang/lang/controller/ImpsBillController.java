5fff52e19692a85114df3d0d14639e28fb3353a1
km611112 <sunyukun>
2019-04-13 15:22:59 +0800

    自定义异与日志的打印

--- src/main/java/cn/luotuoyulang/lang/controller/ImpsBillController.java
+++ src/main/java/cn/luotuoyulang/lang/controller/ImpsBillController.java
@@ -2,9 +2,11 @@ package cn.luotuoyulang.lang.controller;
 
 
 import cn.luotuoyulang.lang.entity.ImpsBill;
+import cn.luotuoyulang.lang.exception.MyRuntimeException;
 import cn.luotuoyulang.lang.service.IImpsBillService;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringEscapeUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -21,6 +23,7 @@ import javax.servlet.http.HttpServletRequest;
  * @author jobob
  * @since 2019-03-20
  */
+@Slf4j
 @RestController
 @RequestMapping("/imps-bill")
 public class ImpsBillController {
@@ -31,17 +34,14 @@ public class ImpsBillController {
     @RequestMapping(value = "/a",method = RequestMethod.GET)
     public String a(String va){
         try {
+            log.info("进来啦   [{}]  " ,va);
             IPage<ImpsBill> page = impsBillService.page(new Page<>(0, 3));
             String sa = StringEscapeUtils.escapeHtml("<a>dddd</a>");
             return va;
-                    
         } catch (Exception e) {
             e.printStackTrace();
-            return null;
+            throw new MyRuntimeException();
         }
     }
 
-    public static void main(String[] args) {
-        System.out.println(StringEscapeUtils.escapeHtml("<a>dddd</a>"));
-    }
 }
